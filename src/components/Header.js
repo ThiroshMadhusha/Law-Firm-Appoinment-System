9e001a2360e1883dc666eb4d8e3ef212f50c4625
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-30 22:17:15 +0530

    Create Header Section in Lawfirm Website

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+// import components
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+
+// import logo
+import Logo from "../assets/img/logo.png";
+
+const Header = () => {
+  const [bg, setBg] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrolly > 80) {
+        setBg(true);
+      } else {
+        setBg(false);
+      }
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        bg && "bg-white shadow-md py-4"
+      } p-8 lg:px-0 w-full fixed z-10 transition-all duration-300`}
+    >
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <a href="#logo">
+          <img src={Logo} alt="" />
+        </a>
+
+        {/* Nav Bar */}
+
+        <Nav />
+
+        {/* Nav Mobile */}
+        <NavMobile />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
