95124458acea35bc05e06b6a53ec557d678108a9
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-30 22:15:38 +0530

    add header component inside to App js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 // import components
+import Header from "./components/Header";
+
 
 const App = () => {
   return (
-  <div>React App</div>
-    )
+    <div>
+      <Header />
+      <div className="h-[2000px"></div>
+  
+    </div>
+  );
 };
 
 export default App;
