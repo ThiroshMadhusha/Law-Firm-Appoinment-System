2ed898a38638014bff2efa845caa04cdc89e01e6
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-01 15:19:36 +0530

    Add Footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-darker py-[24px] text-white text-sm text-center">
+      2022 &copy; Thirosh Madhusha
+    </footer>
+  );
+};
+
+export default Footer;
